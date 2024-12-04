79d637da21842969386a6b9e719ab49443086d88
Marco Wenzel <mar.wenzel@gmx.de>
2024-12-04 18:36:22 +0100

    solve day 3 part 2

--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -7,7 +7,7 @@ import Testing
 struct Day03Tests {
   // Smoke test data provided in the challenge question
   let testData = """
-    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+    xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
     """
 
   @Test
@@ -19,6 +19,6 @@ struct Day03Tests {
   @Test
   func testPart2() async throws {
     let challenge = Day03(data: testData)
-    #expect(String(describing: challenge.part2()) == "0")
+    #expect(String(describing: challenge.part2()) == "48")
   }
 }
